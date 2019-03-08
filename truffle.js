97f1ad2fcebf2affac0e1ceff24cf4ea3111778e
trinhdinhphuong <trinhdinhphuong1@gmail.com>
2019-03-08 11:32:24 +0700

    deploy ok

--- truffle.js
+++ truffle.js
@@ -1,23 +1,22 @@
-require('dotenv').config({path: '.env.local'});
+require('dotenv').config({path: '.env'});
 const Web3 = require("web3");
 const web3 = new Web3();
 const WalletProvider = require("truffle-wallet-provider");
 const Wallet = require('ethereumjs-wallet');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
-	networks: {
-		ropsten: {
-		    provider: function(){
-		    	var ropstenPrivateKey = new Buffer(process.env["ROPSTEN_PRIVATE_KEY"], "hex")
-				var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
-		    	return new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
-		    },
-		    gas: 4600000,
-	      	gasPrice: web3.toWei("20", "gwei"),
-		    network_id: '3',
-		}
-	},
-	compilers: {
+        networks: {
+                ropsten: {
+                    provider: function(){
+                        return new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY)
+                        },
+                         network_id: 3,
+                         gas: 3000000,
+                         gasPrice: 10000000000
+                        }
+                    },
+        compilers: {
     solc: {
        version: "0.4.21"
     }
